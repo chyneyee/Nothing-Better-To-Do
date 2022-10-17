4f5290ba7892147586df69168a35c0c89e6992e7
Yee <windyspringyee@gmail.com>
2022-10-17 16:53:43 +1100

    Create Exercise01_01.java

---
+++ Java/Intro-to-Java-Programming-Data-Structures/Chapter01/Exercise01_01.java
@@ -0,0 +1,39 @@
+/**
+ * @author mychin
+ * Exercise01_01.java 
+ * 14 Oct 2022 4:27:54 pm
+ */
+package chapter01;
+
+
+public class Exercise01_01 
+{
+	public static void main(String[] args) 
+	{
+		// Display message in one line.
+		System.out.println("Welcome to Java, Learning Java Now, Programming is fun.");
+		
+		// Display message in three lines.
+		System.out.println("Welcome to Java,");
+		System.out.println("Learning Java Now,");
+		System.out.println("Programming is fun.");
+		
+		System.out.println();
+		// Display message in three lines using newline character.
+		System.out.println("Welcome to Java, \nLearning Java Now, \nProgramming is fun.");
+	}
+}
+
+
+
+/**
+ * Output
+ * Welcome to Java, Learning Java Now, Programming is fun.
+ * Welcome to Java,
+ * Learning Java Now,
+ * Programming is fun.
+
+ * Welcome to Java, 
+ * Learning Java Now, 
+ * Programming is fun
+*/
