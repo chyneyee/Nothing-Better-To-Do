5b586e10393ab08bcdd7578576dbb11b3a176721
Yee <windyspringyee@gmail.com>
2022-11-16 15:55:24 +1100

    Update Exercise01_05.py

--- Python/Introducing-Python/Chapter01/Exercise01_05.py
+++ Python/Introducing-Python/Chapter01/Exercise01_05.py
@@ -1,5 +1,3 @@
-Python 3.10.7 (v3.10.7:6cc6b13308, Sep  5 2022, 14:02:52) [Clang 13.0.0 (clang-1300.0.29.30)] on darwin
-Type "help", "copyright", "credits" or "license()" for more information.
 >>> # 1.5 Now, type print(47) and press Enter. Did that also print 47 for you on the next line?
 >>> print(47)
 47
