6f3103bae31e59f3201dc7e96d7de3dc022c590f
Yee <windyspringyee@gmail.com>
2022-11-17 14:15:38 +1100

    Add files via upload

---
+++ Python/Introducing-Python/Chapter04/Exercise04_02.py
@@ -0,0 +1,13 @@
+# 4.2 Assign True or False to the variables small and green. Write some if/else statements to print which of these matches those choices: cherry, pea, watermelon, pumpkin.
+
+small = True
+green = False
+if small:
+    if green:
+        print('pea')
+    else:
+        print('cherry')
+elif green:
+        print('watermelon')
+else:
+    print('pumpkin')
