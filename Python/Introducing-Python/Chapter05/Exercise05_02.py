b5739dc28fdd4acefdd11cd4352a97be952d7335
Yee <windyspringyee@gmail.com>
2022-11-19 12:10:53 +1100

    Add files via upload

---
+++ Python/Introducing-Python/Chapter05/Exercise05_02.py
@@ -0,0 +1,25 @@
+# 5.2 Print each list question with its correctly matching answer, in the form:
+# Q: question
+# A: answer
+# >>> questions = [
+# "We don't serve strings around here. Are you a string?",
+# "What is said on Father's Day in the forest?",
+# "What makes the sound 'Sis! Boom! Bah!'?"
+# ]
+# >>> answers = [
+# "An exploding sheep.",
+# "No, I'm a frayed knot.",
+# "'Pop!' goes the weasel."
+# ]
+
+questions = ["We don't serve strings around here. Are you a string?",
+"What is said on Father's Day in the forest?",
+"What makes the sound 'Sis! Boom! Bah!'?"
+]
+answers = ["An exploding sheep.",
+"No, I'm a frayed knot.",
+"'Pop!' goes the weasel."
+]
+print("Q: ",questions[0],"\nA: ",answers[1],
+"\n\nQ: ",questions[1],"\nA: ",answers[2],
+"\n\nQ: ",questions[2],"\nA: ",answers[0])
