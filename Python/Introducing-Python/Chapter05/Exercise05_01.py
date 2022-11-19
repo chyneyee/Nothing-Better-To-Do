04aa69cdc00bdf3fb59431d65c2b4e0459a98fed
Yee <windyspringyee@gmail.com>
2022-11-19 12:10:30 +1100

    Create Exercise05_01.py

---
+++ Python/Introducing-Python/Chapter05/Exercise05_01.py
@@ -0,0 +1,9 @@
+# 5.1 Capitalise the word starting with m:
+# >>> song = """When an eel grabs your arm,
+# And it causes great harm,
+# That's - a moray!"""
+
+song = """When an eel grabs your arm,
+And it causes great harm,
+That's - a moray!"""
+print(song.replace(' m', ' M'))
