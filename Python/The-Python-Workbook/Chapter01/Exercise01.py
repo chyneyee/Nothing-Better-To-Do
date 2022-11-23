a8b47b2d56ebc1efc880cbc7f3d53d7bc0885388
Yee <windyspringyee@gmail.com>
2022-11-23 11:00:34 +1100

    Create Exercise01.py

---
+++ Python/The-Python-Workbook/Chapter01/Exercise01.py
@@ -0,0 +1,8 @@
+# 23-11-2022 9:19 am
+# Exercise 1: Mailing Address
+# Create a program that displays your name and complete mailing address. 
+# The address should be printed in the format that is normally used in the area where you live. 
+# Your program does not need to read any input from the user.
+
+print('Andrew Aguecheek')
+print('2-1 Balsam Hill,\nChatswood.\nLondon EC1A 1HQ')
