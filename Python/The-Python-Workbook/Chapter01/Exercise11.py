a3d1940bdda2a07dc77ebfb5bc559e06b985ac4a
Yee <windyspringyee@gmail.com>
2022-11-24 10:54:26 +1100

    Update Exercise11.py

--- Python/The-Python-Workbook/Chapter01/Exercise11.py
+++ Python/The-Python-Workbook/Chapter01/Exercise11.py
@@ -1,6 +1,9 @@
 # 24-11-2022 10:39 am
 # Exercise 11: Fuel Efficiency
-# In the United States, fuel efficiency for vehicles is normally expressed in miles-per-gallon (MPG). In Canada, fuel efficiency is normally expressed in litres-per-hundred kilometres (L/100 km). Use your research skills to determine how to convert from MPG to L/100 km. Then create a program that reads a value from the user in American units and displays the equivalent fuel efficiency in Canadian units.
+# In the United States, fuel efficiency for vehicles is normally expressed in miles-per-gallon (MPG). 
+# In Canada, fuel efficiency is normally expressed in litres-per-hundred kilometres (L/100 km). 
+# Use your research skills to determine how to convert from MPG to L/100 km. 
+# Then create a program that reads a value from the user in American units and displays the equivalent fuel efficiency in Canadian units.
 
 # Reads user's input
 mpg = float(input("Enter miles per gallon: "))
