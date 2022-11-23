3b993b67855fe357af3b731f5af55546f6627a70
Yee <windyspringyee@gmail.com>
2022-11-23 14:40:14 +1100

    Update Exercise05.py

--- Python/The-Python-Workbook/Chapter01/Exercise05.py
+++ Python/The-Python-Workbook/Chapter01/Exercise05.py
@@ -1,6 +1,7 @@
 # 23-11-2022 2:27 pm
 # Exercise 5: Bottle Deposits
-# In many jurisdictions a small deposit is added to drink containers to encourage people to recycle them. In one particular jurisdiction, drink containers holding one litre or less have a $0.10 deposit, and drink containers holding more than one litre have a $0.25 deposit.
+# In many jurisdictions a small deposit is added to drink containers to encourage people to recycle them. 
+# In one particular jurisdiction, drink containers holding one litre or less have a $0.10 deposit, and drink containers holding more than one litre have a $0.25 deposit.
 # Write a program that reads the number of containers of each size from the user. Your program should continue by computing and displaying the refund that will be received for returning those containers. Format the output so that it includes a dollar sign and two digits to the right of the decimal point.
 
 # Define constraints
