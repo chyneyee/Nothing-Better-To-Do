5bdcf9457937fdaac1a4b20f69312a631ecfa6da
Yee <windyspringyee@gmail.com>
2022-11-24 11:38:48 +1100

    Update Exercise12.py

--- Python/The-Python-Workbook/Chapter01/Exercise12.py
+++ Python/The-Python-Workbook/Chapter01/Exercise12.py
@@ -6,8 +6,10 @@
 # The distance between these points, following the surface of the Earth, in kilometers is: 
 # distance = 6371.01 × arccos(sin(t1) × sin(t2) + cos(t1) × cos(t2) × cos(g1 − g2)).
 # The value 6371.01 in the previous equation wasn't selected at random. It is the average radius of the Earth in kilometres.
-# Create a program that allows the user to enter the latitude and longitude of two points on the Earth in degrees. Your program should display the distance between the points, following the surface of the earth, in kilometers.
-# Hint: Python's trigonometric functions operate in radians. As a result, you will need to convert the user's input from degrees to radians before computing the distance with the formula discussed previously. The math module contains a function named radians which converts from degrees to radians.
+# Create a program that allows the user to enter the latitude and longitude of two points on the Earth in degrees. 
+# Your program should display the distance between the points, following the surface of the earth, in kilometers.
+# Hint: Python's trigonometric functions operate in radians. 
+# As a result, you will need to convert the user's input from degrees to radians before computing the distance with the formula discussed previously. The math module contains a function named radians which converts from degrees to radians.
 
 import math
 
