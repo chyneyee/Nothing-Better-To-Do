ed4877de9f4f3a3d854cfd4bfa9e9e74b14d9fd8
Yee <windyspringyee@gmail.com>
2022-11-24 17:43:43 +1100

    Add files via upload

---
+++ Python/The-Python-Workbook/Chapter01/Exercise26.py
@@ -0,0 +1,11 @@
+# 24-11-2022 5:39 pm
+# Exercise 26: Current Time
+# Python's time module includes several time-related functions. One of these is the asctime function which reads the current time
+# from the computer's internal clock and returns it in a human-readable format. 
+# Use this function to write a program that displays the current time and date.
+# Your program will not require any input from the user.
+
+import time
+
+# Display the current time and date
+print("The current time and date is",time.asctime())
