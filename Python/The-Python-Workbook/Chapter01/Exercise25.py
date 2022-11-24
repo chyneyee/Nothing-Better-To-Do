f762be5c1ef81dd4fa4dcc3837d7b42e91405028
Yee <windyspringyee@gmail.com>
2022-11-24 17:17:40 +1100

    Update Exercise25.py

--- Python/The-Python-Workbook/Chapter01/Exercise25.py
+++ Python/The-Python-Workbook/Chapter01/Exercise25.py
@@ -1,7 +1,9 @@
 # 24-11-2022 4:53 pm
 # Exercise 25: Units of Time (Again)
 # In this exercise you will reverse the process described in the previous exercise.
-# Develop a program that begins by reading a number of seconds from the user. Then your program should display the equivalent amount of time in the form D:HH:MM:SS, where D, HH, MM, and SS represent days, hours, minutes and seconds respectively. 
+# Develop a program that begins by reading a number of seconds from the user. 
+# Then your program should display the equivalent amount of time in the form D:HH:MM:SS, where D, HH, MM, and SS represent days, hours, minutes 
+# and seconds respectively. 
 # The hours, minutes and seconds should all be formatted so that they occupy exactly two digits, with a leading 0 displayed if necessary.
 # Use your research skills determine what additonal character needs to be included in the format specifier so that leading zeros are used instead of leading spaces when a number is formatted to particular width.
 
