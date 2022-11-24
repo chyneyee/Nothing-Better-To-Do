4d2610a709f2d34bc388e647fc1f792b58e63051
Yee <windyspringyee@gmail.com>
2022-11-24 16:05:21 +1100

    Update Exercise22.py

--- Python/The-Python-Workbook/Chapter01/Exercise22.py
+++ Python/The-Python-Workbook/Chapter01/Exercise22.py
@@ -1,6 +1,7 @@
 # 24-11-2022 3:42 pm
 # Exercise 22: Area of a Triangle (Again)
-# In the previous exercise you created a program that computed the area of a triangle when the length of its base and its height were known. It is also possible to compute the area of a triangle when the lengths of all three sides are known. 
+# In the previous exercise you created a program that computed the area of a triangle when the length of its base and its height were known. 
+# It is also possible to compute the area of a triangle when the lengths of all three sides are known. 
 # Let s1, s2 and s3 be the lengths of the sides. Let s = (s1 + s2 + s3)/2. 
 # Then the area of the triangle can be calculated using the following formula:
 # area = sqrt(s × (s − s1) × (s − s2) × (s − s3))
