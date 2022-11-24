699554a208de32588b087b854d7f754b9ac18f6e
Yee <windyspringyee@gmail.com>
2022-11-24 15:22:33 +1100

    Update Exercise20.py

--- Python/The-Python-Workbook/Chapter01/Exercise20.py
+++ Python/The-Python-Workbook/Chapter01/Exercise20.py
@@ -7,7 +7,8 @@
 # Write a program that computes the amount of gas in moles when the user supplies the pressure, volume and temperature. 
 # Test your program by determining the number of moles of gas in a SCUBA tank. A typical SCUBA tank holds 12 liters of gas at
 # a pressure of 20,000,000 Pascals (approximately 3,000 PSI). Room temperature is approximately 20 degrees Celsius or 68 degrees Fahrenheit.
-# Hint: A temperature is converted from Celsius to Kelvin by adding 273.15 to it. To convert a temperature from Fahrenheit to Kelvin, deduct 32 from it, multiply it by 5 / 9 and then add 273.15 to it.
+# Hint: A temperature is converted from Celsius to Kelvin by adding 273.15 to it. To convert a temperature from Fahrenheit to Kelvin, 
+# deduct 32 from it, multiply it by 5 / 9 and then add 273.15 to it.
 
 R = 8.314
 Kelvin = 273.15
