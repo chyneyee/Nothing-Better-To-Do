4e31ce7327b86ff7028990163f823ea6c6e88120
Yee <windyspringyee@gmail.com>
2022-11-29 13:49:42 +1100

    Update Exercise36.py

--- Python/The-Python-Workbook/Chapter02/Exercise36.py
+++ Python/The-Python-Workbook/Chapter02/Exercise36.py
@@ -1,7 +1,10 @@
 # 29-11-2022 12:06 pm
 # Exercise 36: Dog Years
-# It is commonly said that one human year is equivalent to 7 dog years. However this simple conversion fails to recognize that dogs reach adulthood in approximately two years. As a result, some people believe that it is better to count each of the first two human years as 10.5 dog years, and then count each additional human year as 4 dog years.
-# Write a program that implements the conversion from human years to dog years described in the previous paragraph. Ensure that your program works correctly for conversions of less than two human years and for conversions of two or more human years. 
+# It is commonly said that one human year is equivalent to 7 dog years. However this simple conversion fails to recognize that dogs reach adulthood in
+# approximately two years. As a result, some people believe that it is better to count each of the first two human years as 10.5 dog years, and then count
+# each additional human year as 4 dog years.
+# Write a program that implements the conversion from human years to dog years described in the previous paragraph. Ensure that your program works
+# correctly for conversions of less than two human years and for conversions of two or more human years. 
 # Your program should display an appropriate error message if the user enters a negative number.
 
 # Prompts the user for input
