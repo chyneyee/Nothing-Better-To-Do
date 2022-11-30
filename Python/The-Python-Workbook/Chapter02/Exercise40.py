dab6f271fd749afd22d9bf5cee1dd40f94e56eec
Yee <windyspringyee@gmail.com>
2022-11-30 16:15:39 +1100

    Update Exercise40.py

--- Python/The-Python-Workbook/Chapter02/Exercise40.py
+++ Python/The-Python-Workbook/Chapter02/Exercise40.py
@@ -1,7 +1,8 @@
 # 30-11-2022 4:00 pm
 # Exercise 40: Sound Levels
 # The following table lists the sound level in decibels for several common noises.
-# Write a program that reads a sound level in decibels from the user. If the user enters a decibel level that matches one of the noises in the table then your program should display a message containing only that noise. 
+# Write a program that reads a sound level in decibels from the user. If the user enters a decibel level that matches one of the noises in the table then
+# your program should display a message containing only that noise. 
 # If the user enters a number of decibels between the noises listed then your program should display a message indicating which noises the level is between. 
 # Ensure that your program also generates reasonable output for a value smaller than the quietest noise in the table, and for a
 # value larger than the loudest noise in the table.
