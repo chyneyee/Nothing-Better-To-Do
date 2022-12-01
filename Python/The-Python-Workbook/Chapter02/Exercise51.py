9fe26007e863814dca3788950b6d850802a2f11d
Yee <windyspringyee@gmail.com>
2022-12-01 18:40:10 +1100

    Update Exercise51.py

--- Python/The-Python-Workbook/Chapter02/Exercise51.py
+++ Python/The-Python-Workbook/Chapter02/Exercise51.py
@@ -6,7 +6,8 @@
 # root = −b ± √b2 − 4ac / 2a
 # The portion of the expression under the square root sign is called the discriminant.
 # If the discriminant is negative then the quadratic equation does not have any real roots.
-# If the discriminant is 0, then the equation has one real root. Otherwise the equation has two real roots, and the expression must be evaluated twice, once using a plus sign, and once using a minus sign, when computing the numerator.
+# If the discriminant is 0, then the equation has one real root. Otherwise the equation has two real roots, and the expression must be evaluated twice, 
+# once using a plus sign, and once using a minus sign, when computing the numerator.
 # Write a program that computes the real roots of a quadratic function. 
 # Your program should begin by prompting the user for the values of a, b and c. 
 # Then it should display a message indicating the number of real roots, along with the values of the real roots (if any).
@@ -18,6 +19,7 @@ a = int(input("Enter value for a: "))
 b = int(input("Enter value for b: "))
 c = int(input("Enter value for c: "))
 
+# Calculate the discriminant and determines the root
 discriminant = math.pow(b,2) - (4 * a * c)
 if discriminant < 0:
     print("The quadratic equation does not have any real roots.")
