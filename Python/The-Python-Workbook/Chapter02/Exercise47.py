b441f0528341060341de8b0d9523d27c988282a1
Yee <windyspringyee@gmail.com>
2022-12-01 16:16:42 +1100

    Update Exercise47.py

--- Python/The-Python-Workbook/Chapter02/Exercise47.py
+++ Python/The-Python-Workbook/Chapter02/Exercise47.py
@@ -1,13 +1,16 @@
 # 01-12-2022 3:56 pm
 # Exercise 47: Season from Month and Day
-# The year is divided into four seasons: spring, summer, fall and winter. While the exact dates that the seasons change vary a little bit from year to year because of the way that the calendar is constructed, we will use the following dates for this exercise:
+# The year is divided into four seasons: spring, summer, fall and winter. 
+# While the exact dates that the seasons change vary a little bit from year to year because of the way that the calendar is constructed, 
+# we will use the following dates for this exercise:
 # Season  First day
 # Spring  March 20
 # Summer  June 21
 # Fall    September 22
 # Winter  December 21
 # Create a program that reads a month and day from the user. 
-# The user will enter the name of the month as a string, followed by the day within the month as an integer. Then your program should display the season associated with the date that was entered.
+# The user will enter the name of the month as a string, followed by the day within the month as an integer. 
+#Then your program should display the season associated with the date that was entered.
 
 # Prompts user for the input
 month = input("Enter the month: ")
