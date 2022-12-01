c674df51b1d1b0d1c8e41305b234d261aea3d2f5
Yee <windyspringyee@gmail.com>
2022-12-01 14:49:13 +1100

    Update Exercise45.py

--- Python/The-Python-Workbook/Chapter02/Exercise45.py
+++ Python/The-Python-Workbook/Chapter02/Exercise45.py
@@ -5,7 +5,8 @@
 # New year’s day      January 1
 # Canada day          July 1
 # Christmas day       December 25
-# Write a program that reads a month and day from the user. If the month and day match one of the holidays listed previously then your program should display the holiday’s name. 
+# Write a program that reads a month and day from the user. If the month and day match one of the holidays listed previously 
+# then your program should display the holiday’s name. 
 # Otherwise your program should indicate that the entered month and day do not correspond to a fixed-date holiday.
 
 # Prompts input from user
