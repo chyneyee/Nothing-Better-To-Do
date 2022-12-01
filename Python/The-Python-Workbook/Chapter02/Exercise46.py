202fcb752e3084301f1cdaf6bcaf5640845a513f
Yee <windyspringyee@gmail.com>
2022-12-01 15:56:03 +1100

    Update Exercise46.py

--- Python/The-Python-Workbook/Chapter02/Exercise46.py
+++ Python/The-Python-Workbook/Chapter02/Exercise46.py
@@ -2,7 +2,9 @@
 # Exercise 46: What Color Is That Square?
 # Positions on a chess board are identified by a letter and a number. The letter identifies the column, while the number identifies the row, as shown below.
 # Write a program that reads a position from the user. Use an if statement to determine if the column begins with a black square or a white square. 
-# Then use modular arithmetic to report the color of the square in that row. For example, if the user enters a1 then your program should report that the square is black. If the user enters d5 then your program should report that the square is white. 
+# Then use modular arithmetic to report the color of the square in that row. 
+# For example, if the user enters a1 then your program should report that the square is black. 
+# If the user enters d5 then your program should report that the square is white. 
 # Your program may assume that a valid position will always be entered. It does not need to perform any error checking.
 
 # Prompts for input from user
