d906e955024bc6893e9c213f1ed49d0130d4894d
Yee <windyspringyee@gmail.com>
2023-06-23 17:29:08 +1000

    Create Exercise06_01.py

---
+++ Python/Introducing-Python/Chapter06/Exercise06_01.py
@@ -0,0 +1,12 @@
+# 6.1 Use a for loop to print the values of the list [3, 2, 1, 0].
+
+# First method
+for x in range(3, -1, -1):
+    print (x)
+
+print ("\n")
+
+# Second method
+numbers = ['3', '2', '1', '0']
+for number in numbers:
+    print (number)
